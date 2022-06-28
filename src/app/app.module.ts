23cdaad95f4a51d559ef5c9ba654904c3d75e6b9
Iysha Barton <iyshabarton@MacBook-Pro-de-Iysha.local>
2022-06-28 14:52:56 +0200

    have generated new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FaceSnapComponent } from './face-snap/face-snap.component';
+import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaceSnapComponent,
+    FaceSnapListComponent
   ],
   imports: [
     BrowserModule
